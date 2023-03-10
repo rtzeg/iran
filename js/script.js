1418310c2a7cab7304759731fbf572d90b90c1a2
shakhawe <shakhaweb@gmail.com>
2023-03-11 00:43:15 +0500

    add-slider

--- js/script.js
+++ js/script.js
@@ -57,6 +57,26 @@ function scrool_js3(event) {
 }
 
 
-
-
 lnk3.addEventListener('click', scrool_js3)
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
