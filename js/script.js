53a2fdc938d6d789ddeddc6f363370dd06813881
JokerJF <djokerd09@gmail.com>
2023-03-11 15:04:06 +0300

    shrek_radylsa

--- js/script.js
+++ js/script.js
@@ -4,12 +4,12 @@ let nm = document.querySelector('header')
 
 let promo = nm.offsetTop
 
-function scrol1(event){
-    event.preventDefault()
-    window.scroll({
-        top:promo,
-        behavior:"smooth",
-    })
+function scrol1(event) {
+  event.preventDefault()
+  window.scroll({
+    top: promo,
+    behavior: "smooth",
+  })
 }
 
 mn.addEventListener('click', scrol1)
@@ -24,12 +24,12 @@ let sec2_pos = cont.offsetTop
 
 
 function scrool_js1(event) {
-    event.preventDefault()
-    cont.scrollIntoView({       
-    behavior:'smooth',
-    block:'start',
-    inline:'end'
-    })
+  event.preventDefault()
+  cont.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+    inline: 'end'
+  })
 }
 
 
@@ -48,12 +48,12 @@ let sec3_pos = sec.offsetTop
 
 
 function scrool_js3(event) {
-    event.preventDefault()
-    sec.scrollIntoView({       
-    behavior:'smooth',
-    block:'start',
-    inline:'end'
-    })
+  event.preventDefault()
+  sec.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+    inline: 'end'
+  })
 }
 
 
@@ -103,3 +103,30 @@ let close_modal = () => {
 open_btn.addEventListener('click', open_modal)
 close_btn.addEventListener('click', close_modal)
 // конец скрипта для футера 
+
+
+// провека размера для изменения картин
+function zero_time() {
+  let wind = window.screen.width
+  let slid_box_img_1 = document.querySelector('.slid_box .img_1 img')
+  let slid_box_img_2 = document.querySelector('.slid_box .img_2 img')
+  if (wind <= 768) {
+    slid_box_img_1.src = './img/z1_1.png'
+    slid_box_img_2.src = './img/z2_1.png'
+  }else if (wind > 768) {
+    slid_box_img_1.src = './img/z1.png'
+    slid_box_img_2.src = './img/z2.png'
+  }
+}
+
+setInterval(zero_time, 500);
+
+// бургер 
+let burger = document.querySelector('.burger')
+let hed_nav = document.querySelector('.hed nav')
+burger.addEventListener('click',()=>{
+  // burger.classList.add('avt_block')
+  console.log('ss');
+  hed_nav.classList.toggle('nav_right')
+
+})
